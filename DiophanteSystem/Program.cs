85d074c152954c742c3dd896ee69baa9f6d2ac9e
Витя <vitia.iezhovqqqq@gmail.com>
2025-09-14 14:39:22 +0300

    Обобщил на несколько строк. Попытался учесть случай equations > variables.

--- DiophanteSystem/Program.cs
+++ DiophanteSystem/Program.cs
@@ -1,63 +1,121 @@
 ﻿using DiophanteSystem;
 
-static void NormalizeRow(int [,] a, int row, int variablesCount, int equationsCount)
-{
-    var greatestCommonDivisor = MathInt.GreatestCommonDivisor(a[row, 0],  a[row, 1]);
-    for (var i = 2; i < variablesCount; i++)
-    {
-        greatestCommonDivisor = MathInt.GreatestCommonDivisor(greatestCommonDivisor, a[row, i]);
-    }
 
+static void NormalizeRow(int[,] a, int row, int variablesCount, int equationsCount)
+{
+    var greatestCommonDivisor = 0;
+    for (var column = row; column < variablesCount; column++)
+        greatestCommonDivisor = MathInt.GreatestCommonDivisor(greatestCommonDivisor, a[row, column]);
+    
     var freeCoefficient = a[row, variablesCount];
+
+    if (greatestCommonDivisor == 0)
+    {
+        if (freeCoefficient != 0)
+            throw new InvalidOperationException("Inconsistent equation: 0 = nonzero");
+    }
     var (_, remainder) = MathInt.Divide(freeCoefficient, greatestCommonDivisor);
     if (remainder != 0)
     {
-        throw new Exception($"Can't normalize row {row}. GCD: {greatestCommonDivisor}, freeCoefficient: {freeCoefficient}");
+        throw new Exception(
+            $"Can't normalize row {row}. GCD: {greatestCommonDivisor}, freeCoefficient: {freeCoefficient}");
     }
 
-    if (row > 0)
-        throw new NotImplementedException();
-
-    while (a.Row(row).SkipLast(1).Count(x => x != 0) > row + 1)
+    while (a.Row(row).Skip(row).SkipLast(1).Count(x => x != 0) > 1)
     {
         var min = a.Row(row)
             .SkipLast(1)
             .Select((value, index) => (value, index))
+            .Skip(row)
             .Where(x => x.value != 0)
             .MinBy(x => Math.Abs(x.value));
-        
+
         var another = a.Row(row)
             .SkipLast(1)
             .Select((value, index) => (value, index))
+            .Skip(row)
             .First(x => x.value != 0 && x.index != min.index);
-        
+
         var (q, _) = MathInt.Divide(another.value, min.value);
-        
+
         a.AddColumn(another.index, min.index, -q);
     }
 
     var lastNonZero = a.Row(row)
         .SkipLast(1)
         .Select((value, index) => (value, index))
-        .First(x => x.value != 0);
-    
+        .Last(x => x.value != 0);
+
     var (p, r) = MathInt.Divide(freeCoefficient, lastNonZero.value);
-    if (r == 0)
+    if (r != 0)
     {
-        throw new Exception($"Can't normalize row {row}. Last: {lastNonZero.value} at {lastNonZero.index}. FreeCoefficient: {freeCoefficient}");
+        throw new Exception(
+            $"Can't normalize row {row}. Last: {lastNonZero.value} at {lastNonZero.index}. FreeCoefficient: {freeCoefficient}");
     }
-    
+
     a.AddColumn(variablesCount, lastNonZero.index, -p);
+
+    if (lastNonZero.index > row)
+    {
+        a.SwapColumns(row, lastNonZero.index); // put on diaginal;
+    }
 }
 
+static Solution Solve(int equationsCount, int[,] ints, int variablesCount)
+{
+    try
+    {
+        for (var i = 0; i < equationsCount; i++)
+        {
+            NormalizeRow(ints, i, variablesCount, equationsCount);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return Solution.Empty;
+    }
+
+
+    var particular = new int[variablesCount];
+    var freeCoefficients = new int[variablesCount, variablesCount - equationsCount];
+    for (var i = 0; i < variablesCount; i++)
+    {
+        particular[i] = ints[equationsCount + i, variablesCount];
+    }
+
+    for (var row = 0; row < variablesCount; row++)
+    {
+        for (var column = 0; column < variablesCount - equationsCount; column++)
+        {
+            freeCoefficients[row, column] = ints[row + equationsCount, column + variablesCount - 1];
+        }
+    }
+
+    var solution1 = new Solution(particular, freeCoefficients);
+    return solution1;
+}
 
 var variablesCount = 3;
-var equationsCount = 2;
+var equationsCount = 4;
 
-var coefficients = new [,] // equationsCount, variablesCount + 1
+// var coefficients = new[,] // equationsCount, variablesCount + 1
+// {
+//     { 3, 4, 0, -8 },
+//     { 7, 0, 5, -6 },
+// };
+// var coefficients = new[,] // equationsCount, variablesCount + 1
+// {
+//     { 2, 1, 4, -3 },
+//     { 3, 5, 7, -8 },
+// };
+
+var coefficients = new[,] // equationsCount, variablesCount + 1
 {
-    {3, 4, 0, -8},
-    {7, 0, 5, -6},
+    { 0, 1, 0, 2 },
+    { 0, 0, 1, -3 },
+    { 1, 0, 0, -12 },
+    { 0, 0, 1, -3 },
 };
 
 var matrix = new int[equationsCount + variablesCount, variablesCount + 1];
@@ -75,9 +133,6 @@ for (var i = 0; i < variablesCount; i++)
     matrix[i + equationsCount, i] = 1;
 }
 
+var solution = Solve(equationsCount, matrix, variablesCount);
 
-NormalizeRow(matrix, 0, variablesCount, equationsCount);
-
-Console.WriteLine("Hello, World!");
-
-
+Console.WriteLine("End");
---
+++ DiophanteSystem/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace DiophanteSystem;
+
+public readonly record struct Solution(int[] Particular, int[,] FreeVariables)
+{
+    public static Solution Empty => new([], new int[0,0]);
+    public int FreeVariablesCount => FreeVariables.GetLength(1);
+    public bool IsEmpty => Particular.Length == 0;
+}
